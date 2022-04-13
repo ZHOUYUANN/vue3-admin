8d689c29bf8eb2c9c935e0e38fedbc333b21683e
ZHOUYUANN <464814413@qq.com>
2022-04-13 21:16:42 +0800

    新增全局配置

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+
+import 'element-plus/theme-chalk/index.css'
+
+// 全局样式文件
+import '@/assets/stylus/index.styl'
 
 // 全局导入 svg 图标
 import installIcon from '@/icons'
@@ -8,4 +14,4 @@ import installIcon from '@/icons'
 const app = createApp(App)
 installIcon(app)
 
-app.use(router).mount('#app')
+app.use(store).use(router).mount('#app')
