9a2853bcac4b60df1b50cdac0d3a84cdca6bba19
ZHOUYUANN <464814413@qq.com>
2022-04-13 21:18:29 +0800

    新增 svg 组件

--- src/components/svg-icon/index.vue
+++ src/components/svg-icon/index.vue
@@ -1,7 +1,41 @@
 <template>
-  <div class="">1</div>
+  <!--
+    两种情况
+    1. 可以选择展示外部的图标
+    2. 可以选择展示内部的图标
+    通过加载源是否有 http:// 或者 https:// 来判断是否是外部资源
+  -->
+  <div v-if="flag" class="">11</div>
+  <svg v-else class="svg-icon" :class="className" aria-hidden="true">
+    <use :xlink:href="innerIconClass" />
+  </svg>
 </template>
 
-<script setup></script>
+<script setup>
+import { defineProps, computed } from 'vue'
+import { isExternal } from '@/utils/utils'
 
-<style lang="stylus" scoped></style>
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true
+  },
+  className: {
+    type: String,
+    default: ''
+  }
+})
+// 判断是否加载外部资源
+const flag = computed(() => isExternal(props.icon))
+
+const innerIconClass = computed(() => `#icon-${props.icon}`)
+</script>
+
+<style lang="stylus" scoped>
+.svg-icon
+  width 1em
+  height 1em
+  vertical-align -0.15em
+  fill currentColor
+  overflow hidden
+</style>
