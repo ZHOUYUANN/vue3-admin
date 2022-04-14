924cfa948d2c96fb1ff703de7cc5cdc25a05b01f
ZHOUYUANN <464814413@qq.com>
2022-04-14 16:25:29 +0800

    修改 svg 组件文件

--- src/components/svg-icon/index.vue
+++ src/components/svg-icon/index.vue
@@ -1,11 +1,15 @@
+<!--
+  两种情况
+  1. 可以选择展示外部的图标
+  2. 可以选择展示内部的图标
+  通过加载源是否有 http:// 或者 https:// 来判断是否是外部资源
+-->
 <template>
-  <!--
-    两种情况
-    1. 可以选择展示外部的图标
-    2. 可以选择展示内部的图标
-    通过加载源是否有 http:// 或者 https:// 来判断是否是外部资源
-  -->
-  <div v-if="flag" class="">11</div>
+  <div
+    v-if="flag"
+    class="svg-icon external-icon"
+    :style="externalIconStyle"
+  ></div>
   <svg v-else class="svg-icon" :class="className" aria-hidden="true">
     <use :xlink:href="innerIconClass" />
   </svg>
@@ -28,6 +32,13 @@ const props = defineProps({
 // 判断是否加载外部资源
 const flag = computed(() => isExternal(props.icon))
 
+const externalIconStyle = computed(() => {
+  return {
+    mask: `url(${props.icon}) no-repeat 50% 50%`,
+    '-webkit-mask': `url(${props.icon}) no-repeat 50% 50%`
+  }
+})
+
 const innerIconClass = computed(() => `#icon-${props.icon}`)
 </script>
 
@@ -38,4 +49,8 @@ const innerIconClass = computed(() => `#icon-${props.icon}`)
   vertical-align -0.15em
   fill currentColor
   overflow hidden
+  &.external-icon
+    display inline-block
+    background-color currentColor
+    mask-size cover !important
 </style>
